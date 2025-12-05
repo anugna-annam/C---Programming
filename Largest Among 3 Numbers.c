3f6867b1b5ab01730bbf1b35a8380549f669f0b8
anugna-annam <annamanugna@gmail.com>
2025-12-05 17:44:35 +0530

    Largest Among 3 Numbers.c

---
+++ Largest Among 3 Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int x,a,b;
+int t;
+scanf("%d%d%d",&x,&a,&b);
+t= (a*1) + (b*2);
+if (t > x)
+{
+printf("Qualify");
+}
+else
+{
+printf("Not Qualify");
+}
+return 0;
+}
